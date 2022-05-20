72331f50d0c3a43db2f6621cc2daa6595d781cf8
Marina Kaeufer <marishutka@yahoo.com>
2022-05-20 19:04:36 -0400

    added prompt and query

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -11,3 +12,42 @@ connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL Server!');
 });
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'employee_choices',
+      message: 'What would you like to do?',
+      choices: ['Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
+    },
+  ])
+  .then(answers => {
+    console.info('Answer:', answers.employee_choices);
+
+    if(answers.employee_choices === 'Add Employee') {
+
+    }
+    else if(answers.employee_choices === 'Update Employee Role'){
+
+    }
+    else if(answers.employee_choices === 'View All Roles'){
+        connection.query('SELECT * FROM roles', (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result);
+        });
+    }
+    else if(answers.employee_choices === 'Add Role'){
+        
+    }
+    else if (answers.employee_choices === 'View All Departments'){
+        
+    }
+    else if (answers.employee_choices === 'Add Department'){
+        
+    } else {
+
+    }
+  });
--- seeds.sql
+++ seeds.sql
@@ -8,16 +8,16 @@ VALUES
 INSERT INTO roles (job_title, salary, department)
 VALUES 
   ("Software Developer", 12.1, 3),
-  ("Title 2", 12.1, 3),
-  ("Title 3", 12.1, 2),
-  ("Title 4", 12.1, 1);
+  ("Cashier", 70.2, 12),
+  ("Supervisor", 93.5, 7),
+  ("Buyer", 79.7, 5);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-  (" Manager Jack", "Dorsey", 1, NULL),
-  ("Manager name 1", "name 2", 2, NULL),
-  ("Manager name 3", "name 4", 3, NULL),
-  ("Manager name 5", "name 6", 4, NULL);
+  ("Jack", "Dorsey", 09393, 23, NULL),
+  ("Anna", "Taylor", 83335, 19, NULL),
+  ("Tiffany", "Stone", 28373, 25,  NULL),
+  ("Nicole", "Thompson", 37262, 07, NULL);
 
 -- INSERT INTO employees (first_name, last_name, role_id, manager_id)
 -- VALUES 
